db00d1ec4a4b64a725927c53374ce6404f49858b
Iain Lane <iain.lane@canonical.com>
2016-02-04 12:03:35 +0000

    Try to deploy landscape and ksplice

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 MYDIR=$(dirname "$(readlink -f $0)")
+PROJECT=prod-ue-appstream
 
 VOLUMENAME=mirror
 VOLUMESIZE=500 # GB
@@ -96,59 +97,94 @@ wait_attached() {
     exit 1
 }
 
+subordinate_charms() {
+    echo 'deploying subordinate charms to $1'
+    if ! juju status "${1}" | grep -q ksplice; then
+        juju add-relation ksplice "$1"
+    fi
+
+    if ! juju status "${1}" | grep -q landscape; then
+        juju add-relation landscape-client "$1"
+    fi
+    wait_deployed "$1"
+}
+
+extra_prodstack_configuration() {
+    if [ -n "${PRODSTACK}" ]; then
+        return
+    fi
+    #
+    # install/update basenode into charms
+    #
+    [ -d "$MYDIR/charms/trusty/landscape-client" ] || bzr checkout --lightweight lp:charms/trusty/landscape-client "$MYDIR/charms/trusty/landscape-client"
+    [ -d "$MYDIR/charms/trusty/ksplice" ] || { echo "Please check out ksplice charm to $MYDIR/charms/trusty"; exit 1; }
+
+    [ -d "$MYDIR/basenode" ] || { echo "Please check out basenode into $MYDIR"; exit 1; }
+    for charmdir in $MYDIR/charms/trusty/*; do
+        # ignore subordinate charms
+        if grep -q 'subordinate:.*true' $charmdir/metadata.yaml; then
+            continue
+        fi
+        echo "Installing basenode into $charmdir"
+        rm -rf "$charmdir/exec.d/basenode"
+        mkdir -p "$charmdir/exec.d"
+        cp -r "$MYDIR/basenode" "$charmdir/exec.d"
+    done
+
+    if ! juju status | grep -q ksplice:; then
+        juju deploy --repository "$MYDIR/charms" local:trusty/ksplice
+        juju set ksplice accesskey=$(cat /srv/mojo/LOCAL/mojo-${PROJECT}/canonical-is-ksplice.key)
+        juju set ksplice source=""
+    fi
+    if ! juju status | grep -q landscape-client:; then
+        cat <<EOF >> "$CONFIG_YAML"
+landscape-client:
+  url: https://landscape.is.canonical.com/message-system
+  ping-url: http://landscape.is.canonical.com/ping
+  account-name: standalone
+  registration-key: $(cat /srv/mojo/LOCAL/mojo-prod-${PROJECT}/canonical-is-landscape.key)
+  tags: juju-managed, devops-instance, devops-production
+EOF
+        juju deploy --repository "${MYDIR}/charms" --config "${CONFIG_YAML}" local:trusty/landscape-client
+    fi
+
+    #
+    # deploy bootstrap-node charm
+    #
+
+    if ! juju status | grep -q bootstrap-node:; then
+        juju deploy --repository "${MYDIR}/charms" --to 0 local:trusty/bootstrap-node
+        wait_deployed "bootstrap-node"
+    fi
+    subordinate_charms "bootstrap-node"
+
+    if [ -n "${DEPLOYED_APPSTREAM}" ]; then
+        subordinate_charms "appstream-dep11"
+    fi
+}
+
 if [ -z "$OS_PASSWORD" ]; then
     echo "OS_PASSWORD not set in environment, please source nova rc"
     exit 1
 fi
 
-#
-# install/update basenode into charms
-#
-
-[ -d "$MYDIR/basenode" ] || { echo "Please check out basenode into $MYDIR"; exit 1; }
-for charmdir in $MYDIR/charms/trusty/*; do
-    # ignore subordinate charms
-    if grep -q 'subordinate:.*true' $charmdir/metadata.yaml; then
-        continue
-    fi
-    echo "Installing basenode into $charmdir"
-    rm -rf "$charmdir/exec.d/basenode"
-    mkdir -p "$charmdir/exec.d"
-    cp -r "$MYDIR/basenode" "$charmdir/exec.d"
-done
-
-#
-# deploy bootstrap-node charm
-#
-
-if ! juju status | grep -q bootstrap-node:; then
-    juju deploy --repository "${MYDIR}/charms" --to 0 local:trusty/bootstrap-node
-    wait_deployed bootstrap-node
-    # XXX: add this in prodstack
-    #echo 'deploying subordinate charms to bootstrap-node'
-    #juju add-relation ksplice bootstrap-node
-    #juju add-relation landscape-client bootstrap-node
-fi
+CONFIG_YAML=$(mktemp)
+trap "rm ${CONFIG_YAML}" EXIT INT QUIT HUP PIPE TERM
 
 if juju status appstream-dep11 | grep -q 'agent-state:'; then
     echo 'WARNING: appstream-dep11 already deployed, skipping'
+    APPSTREAM_ALREADY_DEPLOYED=1
 else
-    config_yaml=$(mktemp)
-    trap "rm ${config_yaml}" EXIT INT QUIT PIPE
+    trap "rm ${CONFIG_YAML}" EXIT INT QUIT PIPE
     # temporarily set arches to amd64 only
-    cat << EOF >> "${config_yaml}"
+    cat << EOF >> "${CONFIG_YAML}"
 appstream-dep11:
     ip: ${IP:-162.213.34.169}
     arches: ${ARCHES:-amd64}
     mirror: ${MIRROR:-archive.ubuntu.com}
 EOF
-    juju deploy --repository "${MYDIR}/charms" --config "${config_yaml}" --constraints "cpu-cores=8 mem=8G" local:trusty/appstream-dep11
+    juju deploy --repository "${MYDIR}/charms" --config "${CONFIG_YAML}" --constraints "cpu-cores=8 mem=8G" local:trusty/appstream-dep11
     wait_deployed appstream-dep11
-    # XXX: add this in prodstack
-    #echo 'deploying subordinate charms to autopkgtest-cloud-worker'
-    #juju add-relation ksplice appstream-dep11
-    #juju add-relation landscape-client appstream-dep11
-    #wait_deployed appstream-dep11
     DEPLOYED_APPSTREAM=1
 fi
 
@@ -169,5 +205,11 @@ if [ -n "${DEPLOYED_APPSTREAM}" ]; then
     done
     nova floating-ip-associate ${machine} ${IP}
     juju expose appstream-dep11
+    extra_prodstack_configuration
     echo "Done. Try http://${IP}"
+fi
+
+if [ -n "${APPSTREAM_ALREADY_DEPLOYED}" ]; then
+    # XXX: could try to upgrade here?
+    extra_prodstack_configuration
 fi
